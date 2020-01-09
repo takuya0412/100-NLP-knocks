d07e7b9fe5ef8ab14274de523729005a69996fc8
Takuya <lukefone.story@gmail.com>
2020-01-09 23:45:07 +0900

    sec03-20

---
+++ src/sec03/20.py
@@ -0,0 +1,21 @@
+import json
+
+
+def read_file(title):
+    decoder = json.JSONDecoder()
+    res = {}
+    with open('../resource/jawiki-country.json') as f:
+        line = f.readline()
+        while line:
+            tmp = decoder.raw_decode(line)
+            if tmp[0]['title'] == title:
+                return tmp[0]['text']
+            line = f.readline()
+    print(res)
+
+
+
+
+
+print(read_file('イギリス'))
+
